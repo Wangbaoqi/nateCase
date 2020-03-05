6d0c62e2af62f442ee49068fec0d8761bc6f64be
nate.wang <baoq.wang@eking-tech.com>
2020-03-05 21:38:12 +0800

    first commit

--- deploy.sh
+++ deploy.sh
@@ -12,14 +12,30 @@ set -e
 # 如果你是要部署到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
+
+git branch -a
+# 新建并切换到 gh-pages 分支
+git checkout -b gh-pages
+# 显示有变更的文件
+git status
+# 删除 master 分支
+git branch -d master
+
+# 添加当前目录的所有文件到暂存区
+git add .
+# 提交暂存区到仓库区，并添加代码提交信息
+git commit -m 'first commit'
+
+# 添加远程仓库
+git remote add origin git@github.com:Wangbaoqi/nateCase.git
+# 把本地的 gh-pages 分支推送到 origin 服务器上
+git push origin gh-pages
+
 
 # 如果你想要部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:Wangbaoqi/nateCase.git master
 
 # 如果你想要部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Wangbaoqi/nateCase.git master:gh-pages
+# git push -f git@github.com:Wangbaoqi/nateCase.git master:gh-pages
 
 cd -
